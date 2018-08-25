d1459870d69d75152510adef7b2ce8e4a3b81222
Blessing Orazulume <orazulumeblessing@gmail.com>
2018-08-25 18:40:14 +0100

    added mongoclient to save data, worked on rootQuery

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 const express  = require('express')
 const graphqlHTTP = require('express-graphql')
+const mongoose = require('mongoose')
 const schema = require('./schema/schema')
 const app = express()
+
+const db = require('./config/db');
 const PORT = 4500
 
+mongoose.connect(db.url)
+mongoose.connection.once('open', () => {
+    console.log('connected to database')
+})
+
 app.use('/graphql', graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
 })) // middleware for graphql to interact with rest endpoint
 
 app.listen(PORT, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,32 +1,92 @@
 const graphql = require('graphql')
 const _ = require('lodash')
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt,
+    GraphQLList
+} = graphql
 
-//dummy data
+//dummy data for books
 let books = [
-    { name: 'Miss me', genre: 'sci/Fi', id: '1' },
-    { name: 'Hello madam', genre: 'fantasy', id: '2' },
-    { name: 'Desmond baby', genre: 'sci/Fi', id: '3' }
+    { name: 'Miss me', genre: 'sci/Fi', id: '1', authorId: '1' },
+    { name: 'Hello madam', genre: 'fantasy', id: '2', authorId: '2' },
+    { name: 'Desmond baby', genre: 'sci/Fi', id: '3', authorId: '3' },
+    { name: 'Geez', genre: 'sci/Fi', id: '1', authorId: '2' },
+    { name: 'Foood food', genre: 'history', id: '2', authorId: '3' },
+    { name: 'Mememe', genre: 'sci/Fi', id: '3', authorId: '3' }
+]
+
+//dummy data for author
+let authors = [
+    { name: 'Chibz D', age: 20, id: '1' },
+    { name: 'Maylem madam', age: 40, id: '2' },
+    { name: 'Desmond baby', age: 15, id: '3' }
 ]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                console.log(parent)
+                return _.find(authors, { id: parent.authorId }) 
+            }
+        }
     })
 })
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books: { //graphql list
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return _.filter(books, { authorId: parent.id })
+            }
+        }
+    })
+})
+
 // how to jump into the graph to get our data
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: { // don't worry about the order so no need for wrapping around function
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // tell graphql how to get the data
                 return _.find(books, { id: args.id })
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                // tell graphql how to get the data
+                return _.find(authors, { id: args.id })
+            }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return books
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args) {
+                return authors
             }
         }
     }

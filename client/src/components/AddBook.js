06af25f5aefbbc30afd591f8831d33066377ca36
Blessing Orazulume <orazulumeblessing@gmail.com>
2018-08-27 00:34:44 +0100

    seperated queries from components

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react'
-import { gql } from 'apollo-boost' 
 import { graphql } from 'react-apollo' //bind query to the component
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`
+import { getAuthorsQuery } from '../queries/queries'
 
 class AddBook extends Component {
 
